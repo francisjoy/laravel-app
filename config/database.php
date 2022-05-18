d22354b357a1142ff5db548480ca79688c7c070f
francisjoy <37526638+francisjoy@users.noreply.github.com>
2022-05-18 12:13:23 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -74,16 +74,20 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
-	
-	'mongo' => [
-            'driver' => 'mongo',
-            'host' => env('MONGODB_HOST', 'mongodb-4-4'),
-            'port' => env('MONGODB_PORT', '27017'),
-            'database' => env('MONGODB_DATABASE', 'phpdatabase'),
-            'username' => env('MONGODB_USERNAME', 'php'),
-            'password' => env('MONGODB_PASSWORD', ''),
-        ],
+	'mongodb' => [
+            'driver' => 'mongodb',
+   	    'host' => env('DB_HOST', '127.0.0.1'),
+    	    'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'homestead'),
+    	    'username' => env('DB_USERNAME', 'homestead'),
+     	    'password' => env('DB_PASSWORD', 'secret'),
+            'options' => [
+       			 // here you can pass more settings to the Mongo Driver Manager
+       			 // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
 
+        		'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
+   	    ],
+	],
     ],
 
     /*
